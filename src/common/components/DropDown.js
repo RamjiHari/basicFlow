4c09c4f56462714bc17210291b7b3e740b7235f6
stenyJ <stenycaliens1999@gmail.com>
2021-06-16 18:43:03 +0530

    registration and preview screen

--- src/common/components/DropDown.js
+++ src/common/components/DropDown.js
@@ -4,14 +4,13 @@ import Master from '../utils/DropDownMaster';
 
 export default function DropDown(props) {
 
-    console.log(Master,"sss")
     const[category,setCategory]= React.useState(Master.filter(e=>e.data_key==props.type))
 
     let items = category.map((item,index) => {
     return ( <Picker.Item key={index} label={item.data_code} value={item.data_code} /> )
     })
     return (
-        <Picker    onValueChange={props.handleText}>
+        <Picker    onValueChange={props.handleText} selectedValue={props.value}>
          <Picker.Item label="Options"/>
             {items}
         </Picker>
